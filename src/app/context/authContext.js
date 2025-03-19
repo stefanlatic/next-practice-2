fda244514598cb33636b2b924d3b6a3dc4f237b7
Stefan <120310018+stefanlatic@users.noreply.github.com>
2025-03-19 21:56:46 +0100

    Implementation Firebase Auth for Login

---
+++ src/app/context/authContext.js
@@ -0,0 +1,39 @@
+'use client'
+
+import { createContext, useContext, useEffect, useState }  from "react";
+import { auth } from "@/app/firebase";
+import React from "react";
+
+const AuthContext = createContext({});
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        return auth.onAuthStateChanged(setupInitialUser);
+
+    }, []);
+    const setupInitialUser = async (authUser) => {
+        if(authUser){
+        setUser(authUser);
+        setLoggedIn (true);
+    }
+    setIsLoading(false);
+
+    }
+
+    return (
+
+        <AuthContext.Provider value={{user, loggedIn, isLoading}}>
+            {!isLoading && children}
+        </AuthContext.Provider>
+    )
+}
---
+++ src/app/user/register/page.js
@@ -0,0 +1,28 @@
+'use client'
+import { useAuth } from "@/app/context/authContext";
+import { auth } from "@/app/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
+export default function Register() {
+
+    const { user } = useAuth();
+    console.log(user)
+
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const register = async(e) => {
+        e.preventDefault();
+        console.log(email, password);
+        
+    await createUserWithEmailAndPassword(auth, 'stefanlatic2002@gmail.com', '12345678');
+    }
+
+    return <>
+    <h1>Please Log In</h1>
+    <input type="text" placeholder="email"  onChange={e => setEmail(e.currentTarget.value)}/>
+    <input type="password" placeholder="password"  onChange={e => setPassword(e.currentTarget.value)}/>
+    <button onClick={e => register(e)}>Register</button>
+    </>
+}
